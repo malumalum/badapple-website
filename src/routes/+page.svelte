303083a62fc7d9c4abc367854fc671751db7caae
Uros <ukalabic@protonmail.com>
2023-06-15 23:25:55 +0200

    Fixed logo resizing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,12 +4,25 @@
 
   import Logo from "/src/lib/images/logo.png"
 
+  let innerWidth = 0;
+
+  const fullWidth = 555;
+  $: imgWidth = innerWidth * 0.9;
+
 </script>
+
+<svelte:window bind:innerWidth />
 
   <NavBar />
 
   <div style="margin: auto; height=100vh; text-align:center">
-    <a href="/about"><img src={Logo} alt="logo" width="500" /></a>
+    <a href="/about">
+      {#if innerWidth > fullWidth}
+        <img src={Logo} alt="logo" width="500" />
+      {:else}
+        <img src={Logo} alt="logo" width={imgWidth} />
+      {/if} 
+    </a>
 
     <h6>&copy; 2023 Bad Apple Inc.</h6>
   </div>
